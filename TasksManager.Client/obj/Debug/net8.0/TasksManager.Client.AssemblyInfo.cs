425ba03f1d5a0779bedcb38528232be46aac3df8
Osita <chrisjerry070@gmail.com>
2024-12-11 21:56:32 +0400

    Working on My tasks page

--- TasksManager.Client/obj/Debug/net8.0/TasksManager.Client.AssemblyInfo.cs
+++ TasksManager.Client/obj/Debug/net8.0/TasksManager.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasksManager.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d504bc8a48cd9634db927fbfa55dcb08ead309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c920dd4bfb5bb6187b591391dac5dfc4017a8e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasksManager.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasksManager.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
