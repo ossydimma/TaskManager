bcf1af0bbe188b30f79695f1e7f75b8e766c03e3
Osita <chrisjerry070@gmail.com>
2024-12-18 19:45:16 +0400

    created documentation page and created the left UI

--- TasksManager/obj/Debug/net8.0/TasksManager.AssemblyInfo.cs
+++ TasksManager/obj/Debug/net8.0/TasksManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TasksManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425ba03f1d5a0779bedcb38528232be46aac3df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46c5d305af2557851f57d6b1664a26f7e6ba12d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TasksManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TasksManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
